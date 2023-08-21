04cba38ff7fdfe4f509a049dc16e8e9c43b08716
YanivShahar <yanivshahar97@gmail.com>
2023-08-21 11:50:02 +0300

    added Car class

--- GarageLogic/Car.cs
+++ GarageLogic/Car.cs
@@ -8,5 +8,12 @@ namespace Ex03.GarageLogic
     class Car
     {
         public int Test { get; set; }
+        public string Name { get; set; }
+
+        public Car()
+        {
+            Test = 4;
+            Name = "Yaniv";
+        }
     }
 }
